217a3f8f3e645e598eaf1b0baff108dcdedc324d
jebius76 <91204905+jebius76@users.noreply.github.com>
2024-03-21 20:23:14 -0300

    deploy

--- src/main/java/com/semillero/ubuntu/config/SecurityConfig.java
+++ src/main/java/com/semillero/ubuntu/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                     auth
                             .requestMatchers("/api/v1/auth/**").permitAll()
                             .requestMatchers("/api/v1/hello/**").permitAll()
+                            .requestMatchers("/api/v1/user/**").permitAll()
                             .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                             .anyRequest().authenticated();
                         })
